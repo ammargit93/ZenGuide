6b0362056807f0e38b4d8382ec7f3de21cae4936
ammargit93 <140953295+ammargit93@users.noreply.github.com>
2024-10-02 01:45:32 +0530

    hid firebaseConfig.jsx

--- src/services/firebaseConfig.jsx
+++ src/services/firebaseConfig.jsx
@@ -9,13 +9,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5eR8Eb67bCiGF-sc-SZ8S9_qKkyTSdBg",
-  authDomain: "zenguide-fa81f.firebaseapp.com",
-  projectId: "zenguide-fa81f",
-  storageBucket: "zenguide-fa81f.appspot.com",
-  messagingSenderId: "417879220768",
-  appId: "1:417879220768:web:3ecb610b5bdbfc37cc1245",
-  measurementId: "G-1464K58W1P"
+  apiKey: apikey,
+  authDomain:authDomain ,
+  projectId: proj_id,
+  storageBucket:storagebucket,
+  messagingSenderId: messagingSenderId,
+  appId:appId,
+  measurementId: measurementId
 };
 
 // Initialize Firebase
